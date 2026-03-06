697298ae0390ea4817ee5e21519a73cd0317b466
ncthangg <ncthang2206@gmail.com>
2026-03-06 15:47:32 +0700

    env.config.ts - remove console.log

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,25 +3,18 @@ let baseUrl;
 switch (import.meta.env.MODE) {
     case "development":
         baseUrl = import.meta.env.VITE_DEV_API_URL;
-        console.log("development");
-        console.log(import.meta.env.VITE_DEV_API_URL);
         break;
 
     case "staging":
         baseUrl = import.meta.env.VITE_STAGING_API_URL;
-        console.log("staging");
-        console.log(import.meta.env.VITE_STAGING_API_URL);
         break;
 
     case "production":
         baseUrl = import.meta.env.VITE_PRODUCTION_API_URL;
-        console.log("production");
-        console.log(import.meta.env.VITE_PRODUCTION_API_URL);
         break;
 
     default:
         baseUrl = import.meta.env.VITE_DEV_API_URL;
-        console.log("default");
 }
 
 const EnvConfig = {
