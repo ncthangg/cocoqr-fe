f2215afa1828a2990867a8bf83ca6f9b4321e502
ncthangg <ncthang2206@gmail.com>
2026-03-06 15:07:36 +0700

    Squashed commit of the following:
    commit f2370045d671e64622e59fec07bec64b3b75791e
    Author: ncthangg <ncthang2206@gmail.com>
    Date:   Fri Mar 6 15:06:40 2026 +0700
    
        update env.config.js
    
    commit 52243a78f6a40ce22b81a0124ba2a9a90a71720c
    Author: ncthangg <ncthang2206@gmail.com>
    Date:   Fri Mar 6 09:26:14 2026 +0700
    
        create base

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,4 +1,24 @@
+let baseUrl;
+
+switch (import.meta.env.MODE) {
+    case "development":
+        baseUrl = import.meta.env.VITE_DEV_API_URL;
+        break;
+
+    case "staging":
+        baseUrl = import.meta.env.VITE_STAGING_API_URL;
+        break;
+
+    case "production":
+        baseUrl = import.meta.env.VITE_PRODUCTION_API_URL;
+        break;
+
+    default:
+        baseUrl = import.meta.env.VITE_DEV_API_URL;
+}
+
 const EnvConfig = {
-    API_BASE_URL: import.meta.env.VITE_API_BASE_URL + '/api',
+    API_BASE_URL: baseUrl + "/api",
 };
+
 export default EnvConfig;
