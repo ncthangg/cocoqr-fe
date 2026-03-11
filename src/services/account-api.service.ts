3c8f36f3c58eb9f7c6283fb2c9d8f130e1efdec1
ncthangg <ncthang2206@gmail.com>
2026-03-11 16:20:36 +0700

    update account-api.service

--- src/services/account-api.service.ts
+++ src/services/account-api.service.ts
@@ -1,17 +1,25 @@
 import { ApiConstant } from "../constants/api.constant";
 import type { AccountRes, PagingVM } from "../models/entity.model";
-import { axiosPrivate, axiosPublic } from "../api/axios.instance";
+import { axiosPrivate } from "../api/axios.instance";
 import type { PostAccountReq, PutAccountReq } from "@/models/entity.request.model";
+import type { AccountProvider } from "@/models/enum";
 
 export const accountApi = {
-    getAll: async (pageNumber: number, pageSize: number, sortField: string | null, sortDirection: "asc" | "desc" | null, isActive: boolean | null, searchValue: string | null): Promise<PagingVM<AccountRes>> => {
-        const response = await axiosPublic.get(ApiConstant.BANKINFO.GET_ALL,
+    getAll: async (pageNumber: number, pageSize: number,
+        userId: string | null,
+        sortField: string | null, sortDirection: "asc" | "desc" | null,
+        provider: AccountProvider | null,
+        isActive: boolean | null,
+        searchValue: string | null): Promise<PagingVM<AccountRes>> => {
+        const response = await axiosPrivate.get(ApiConstant.ACCOUNT.GET_ALL,
             {
                 params: {
                     pageNumber: pageNumber,
                     pageSize: pageSize,
+                    userId: userId,
                     sortField: sortField,
                     sortDirection: sortDirection,
+                    provider: provider,
                     isActive: isActive,
                     searchValue: searchValue,
                 }
@@ -20,19 +28,19 @@ export const accountApi = {
         return response.data;
     },
     getById: async (id: string): Promise<AccountRes> => {
-        const response = await axiosPrivate.get(ApiConstant.BANKINFO.GET_BY_ID(id));
+        const response = await axiosPrivate.get(ApiConstant.ACCOUNT.GET_BY_ID(id));
         return response.data;
     },
     post: async (req: PostAccountReq): Promise<string> => {
-        const response = await axiosPrivate.post(ApiConstant.BANKINFO.POST, req);
+        const response = await axiosPrivate.post(ApiConstant.ACCOUNT.POST, req);
         return response.data;
     },
     put: async (id: string, req: PutAccountReq): Promise<string> => {
-        const response = await axiosPrivate.put(ApiConstant.BANKINFO.PUT(id), req);
+        const response = await axiosPrivate.put(ApiConstant.ACCOUNT.PUT(id), req);
         return response.data;
     },
     delete: async (id: string): Promise<string> => {
-        const response = await axiosPrivate.delete(ApiConstant.BANKINFO.DELETE(id));
+        const response = await axiosPrivate.delete(ApiConstant.ACCOUNT.DELETE(id));
         return response.data;
     },
 };
