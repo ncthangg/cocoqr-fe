50667cff4cd3983852eb19afcce5a7a90caa1b66
ncthangg <ncthang2206@gmail.com>
2026-03-24 09:46:50 +0700

    update env.config.ts

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -3,18 +3,22 @@ let baseUrl;
 switch (import.meta.env.MODE) {
     case "development":
         baseUrl = import.meta.env.VITE_DEV_API_URL;
+        console.log("dev", import.meta.env.VITE_DEV_API_URL);
         break;
 
     case "staging":
         baseUrl = import.meta.env.VITE_STAGING_API_URL;
+        console.log("staging", import.meta.env.VITE_STAGING_API_URL);
         break;
 
     case "production":
         baseUrl = import.meta.env.VITE_PRODUCTION_API_URL;
+        console.log("production", import.meta.env.VITE_PRODUCTION_API_URL);
         break;
 
     default:
         baseUrl = import.meta.env.VITE_DEV_API_URL;
+        console.log("default", import.meta.env.VITE_DEV_API_URL);
 }
 
 const EnvConfig = {
